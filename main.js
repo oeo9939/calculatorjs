78280124300d99c49214286f5a4f0b687a072f51
Teo <mgttmich@gmail.com>
2022-11-22 22:26:52 +0200

    Add handle key press, btn, operator, deci, clear

--- main.js
+++ main.js
@@ -13,3 +13,33 @@ const updateDisplay = () => {
 }
 
 updateDisplay();
+
+// Handle key press
+const keys = document.querySelector(".keys");
+keys.addEventListener("click", (event) => {
+    const { target } = event;
+    if (!target.matches("button")) {
+        return;
+    }
+
+    if (target.classList.contains("operator")) {
+        handleOperator(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains("decimal")) {
+        inputDecimal(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains("all-clear")) {
+        resetCalculator();
+        updateDisplay();
+        return;
+    }
+
+    inputDigit(target.value);
+    updateDisplay();
+});
